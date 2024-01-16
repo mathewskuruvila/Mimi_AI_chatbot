a4db29d8652b2bce6921820bad9debd8b249a82d
Mathews <mathewsulike20@gmail.com>
2024-01-16 16:08:22 +0530

    welcome1

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,6 +1,11 @@
 import express from 'express';
 const app = express();
-app.get("/hello", (req, res, next) => {
+//middllewares
+app.use(express.json());
+//connections and listeneres
+app.post("/hello", (req, res, next) => {
+    console.log(req.body.name);
+    return res.send("Hello world");
 });
 app.listen(5000, () => console.log("Server listening on port"));
 //npm run dev
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,8 +2,13 @@ import express from 'express'
 const app = express();
 
 
-app.get("/hello",(req, res ,next) =>{
-    
+//middllewares
+app.use(express.json());
+
+//connections and listeneres
+app.post("/hello",(req, res ,next) =>{
+    console.log(req.body.name);
+    return res.send("Hello world")
 })
 
 app.listen(5000,() => console.log("Server listening on port"))
