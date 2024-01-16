a6bfb5f5593a5b74d87421efa5ebead89174f683
Mathews <mathewsulike20@gmail.com>
2024-01-16 15:21:57 +0530

    first commit

---
+++ backend/dist/index.js
@@ -0,0 +1,7 @@
+import express from 'express';
+const app = express();
+app.get("/hello", (req, res, next) => {
+});
+app.listen(5000, () => console.log("Server listening on port"));
+//npm run dev
+//# sourceMappingURL=index.js.map
---
+++ backend/src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+const app = express();
+
+
+app.get("/hello",(req, res ,next) =>{
+    
+})
+
+app.listen(5000,() => console.log("Server listening on port"))
+
+
+//npm run dev
