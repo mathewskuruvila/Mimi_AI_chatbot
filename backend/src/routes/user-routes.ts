f5a37a1774caac162846ce4fc25ddf58bad18c33
mathews <mathewsulike20@gmail.com>
2024-02-13 17:17:06 +0530

    ho

--- backend/src/routes/user-routes.ts
+++ backend/src/routes/user-routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
+import { getAllUsers } from "../controllers/user-controllers.js";
 
 const userRoutes = Router();
-userRoutes.get ("/",)
+userRoutes.get ("/",getAllUsers)
 export default userRoutes;
