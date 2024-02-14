a60a0ce50bb7b9c88f598928f2f499c5389e878d
mathews <mathewsulike20@gmail.com>
2024-02-14 23:28:10 +0530

    mongoose

---
+++ backend/src/models/User.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+const chatSchema = new mongoose.Schema({
+
+
+});
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    chats:[],
+})
--- backend/src/routes/user-routes.ts
+++ backend/src/routes/user-routes.ts
@@ -2,5 +2,6 @@ import { Router } from "express";
 import { getAllUsers } from "../controllers/user-controllers.js";
 
 const userRoutes = Router();
-userRoutes.get ("/",getAllUsers)
+userRoutes.get ("/",getAllUsers);
+
 export default userRoutes;
