7c3bf844455de48cd327be4f9c049987df678aaf
mathews <mathewsulike20@gmail.com>
2024-02-14 23:46:50 +0530

    mongoose3

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,3 +1,15 @@
-export const getAllUsers= ()=>{
+import { NextFunction, Request, Response } from "express";
+import user from "../models/User.js";
+
+export const getAllUsers = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
     //get all users
-}
+    try {
+        const users = await User.find();
+    } catch(error) {
+
+    }
+};
