4e8a4d4b33cf22af4c4743b1bb0dd411fc517a83
mathews <mathewsulike20@gmail.com>
2024-02-14 23:52:17 +0530

    mongoose3

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-import user from "../models/User.js";
+import User from "../models/User.js";
 
 export const getAllUsers = async (
     req: Request,
@@ -9,7 +9,9 @@ export const getAllUsers = async (
     //get all users
     try {
         const users = await User.find();
+        return res.status(200).json({message:"OK",users});
     } catch(error) {
-
+        console.log(error)
+        return res.status(200).json({message:"Error",cause:error.message});
     }
 };
